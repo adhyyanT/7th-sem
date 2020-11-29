0cc4bb4e47e976ea4679f31ecbee9e87f7b0edc4
adhyyanT <adhyyan321@gmail.com>
2020-11-29 17:36:30 +0530

    xth

---
+++ Compiler-design-LAB/programs/op2.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+char input[10];
+int i=0;
+char lasthandle[7],stack[50],handles[][6]={")E(","E*E","E+E","i","E-E","E/E"};
+int top=0,l;
+char prec[9][9]={
+                            //input//
+            /*stack    +    -   *   /   ^   i   (   )   $  */
+            /*  + */  '>', '>','<','<','<','<','<','>','>',
+            /*  - */  '>', '>','<','<','<','<','<','>','>',
+            /*  * */  '>', '>','>','>','<','<','<','>','>',
+            /*  / */  '>', '>','>','>','<','<','<','>','>',
+            /*  ^ */  '>', '>','>','>','<','<','<','>','>',
+            /*  i */  '>', '>','>','>','>','e','e','>','>',
+            /*  ( */  '<', '<','<','<','<','<','<','>','e',
+            /*  ) */  '>', '>','>','>','>','e','e','>','>',
+            /*  $ */  '<', '<','<','<','<','<','<','<','>',
+    };
+
+int getindex(char c){
+    switch(c){
+        case '+':return 0;
+        case '-':return 1;
+        case '*':return 2;
+        case '/':return 3;
+        case '^':return 4;
+        case 'i':return 5;
+        case '(':return 6;
+        case ')':return 7;
+        case '$':return 8;
+    }
+}
+int shift(){
+    stack[++top]=*(input+i++);
+    stack[top+1]='\0';
+}
+int reduce(){
+    int i,len,found,t;
+    for(i=0;i<6;i++){
+        len=strlen(handles[i]);
+        if(stack[top]==handles[i][0]&&top+1>=len){
+            found=1;
+            for(t=0;t<len;t++){
+                if(stack[top-t]!=handles[i][t]){
+                    found=0;
+                    break;
+                }
+            }
+            if(found==1){
+                stack[top-t+1]='E';
+                top=top-t+1;
+                strcpy(lasthandle,handles[i]);
+                stack[top+1]='\0';
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+void dispstack(){
+    int j;
+    for(j=0;j<=top;j++)
+        printf("%c",stack[j]);
+}
+void dispinput(){
+    int j;
+    for(j=i;j<l;j++)
+        printf("%c",*(input+j));
+}
+int main(){
+    int j;
+    //input=(char*)malloc(50*sizeof(char));
+    //printf("\n\tEnter the input string: ");
+    strcat(input,"i-i+i*(i)$");
+    l=strlen(input);
+    strcpy(stack,"$");
+    printf("Input:%s\n",input);
+    printf("\nSTACK\tINPUT\t\tACTION");
+    while(i<=l){
+        shift();
+        printf("\n");
+        dispstack();
+        printf("\t");
+        dispinput();
+        printf("\t\tShift");
+        if(prec[getindex(stack[top])][getindex(input[i])]=='>'){
+            while(reduce()){
+                printf("\n");
+                dispstack();
+                printf("\t");
+                dispinput();
+                printf("\t\tReduced: E->%s",lasthandle);
+                }
+            }
+        }
+    if(strcmp(stack,"$E$")==0)
+        printf("\n\n\tString is accepted\n\n");
+    else
+        printf("\n\n\tString is not accepted\n\n");
+    return 0;
+}
